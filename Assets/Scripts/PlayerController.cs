bfd3580137e3cf263719e6c5e56178061f7e343c
jesusdlacrz <delacruzjd@uninorte.edu.co>
2023-05-15 18:00:22 -0500

    Optimizacion de codigo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,36 +6,41 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     bool canJump;
+    Rigidbody2D rigid;
+    Animator animator;
+    SpriteRenderer sprite;
+    [SerializeField] float velocity;
+    float moveH;
+    bool facingRight = true;
     void Start()
     {
-
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update(){
-        if (Input.GetKey("left")){
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-600f * Time.deltaTime, 0));
-            gameObject.GetComponent<Animator>().SetBool("moving", true);
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        if (Input.GetKey("right")){
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(600f * Time.deltaTime, 0));
-            gameObject.GetComponent<Animator>().SetBool("moving", true);
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-
-        if (!Input.GetKey("left") && !Input.GetKey("right")){
-            gameObject.GetComponent<Animator>().SetBool("moving", false);
-        }
-
+        moveH = Input.GetAxis("Horizontal");
+        rigid.velocity = new Vector2(moveH* velocity, rigid.velocity.y );
+        if (moveH != 0) animator.SetBool("moving", true);
+        else animator.SetBool("moving", false);
+        if (!facingRight && moveH > 0) flip();
+        if(facingRight && moveH < 0) flip(); 
         if (Input.GetKeyDown("up") && canJump){
             canJump = false;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 150f));
+            rigid.AddForce(new Vector2(0, 150f));
         }
     }
     private void OnCollisionEnter2D(Collision2D collision){
         if (collision.transform.tag == "ground"){
             canJump = true;
         }
+    }
+    void flip()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+
     }
 }
