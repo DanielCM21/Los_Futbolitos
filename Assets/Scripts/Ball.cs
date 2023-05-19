0af83bdf8d01091101f40bbfb1bfe2423104bf61
BrayanGuti <99362850+BrayanGuti@users.noreply.github.com>
2023-05-18 21:38:54 -0500

    Reincio de posiciones al meter gol
    Reincio de posiciones de blaon y jugador al meter gol

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,9 +6,14 @@ public class Ball : MonoBehaviour
 {   
     private GameObject _player;
     private PlayerController playerController;
+    Rigidbody2D rigid;
+    [SerializeField] private GameObject Cordenadas_ball;
+    [SerializeField] private GameObject Cordenadas_PJ1;
     void Start(){
         _player = GameObject.FindGameObjectWithTag("player");
         playerController = _player.GetComponent<PlayerController>();
+        rigid = GetComponent<Rigidbody2D>();
+
     }
 
     void Update(){
@@ -18,6 +23,22 @@ public class Ball : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision){
         if (collision.gameObject.tag == "player"){
             playerController.canShoot = true;
+        }
+        if (collision.gameObject.tag == "Goal_right"){
+            Debug.Log("GOOOL Derecha");
+            transform.position = Cordenadas_ball.transform.position;
+            playerController.transform.position = Cordenadas_PJ1.transform.position;
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = 0;
+        }
+
+        if (collision.gameObject.tag == "Goal_left")
+        {
+            Debug.Log("GOOOL IZQUIERDA");
+            transform.position = Cordenadas_ball.transform.position;
+            playerController.transform.position = Cordenadas_PJ1.transform.position;
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = 0;
         }
     }
 
